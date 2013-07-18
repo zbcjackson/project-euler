7e14ee37fd1e075816380c73067de1f328344f89
Jackson Zhang <zbcjackson@gmail.com>
2013-07-18 16:56:24 +0800

    Use reverse to determine palindrome

--- 4/palindrome.rb
+++ 4/palindrome.rb
@@ -1,14 +1,7 @@
 class Palindrome
 	def determine number
 		number_string = number.to_s
-		if number_string.length > 1
-			(0..(number_string.length / 2)).each do |index|
-				if number_string[index] != number_string[-1 - index]
-					return false
-				end
-			end
-		end
-		true
+		number_string == number_string.reverse
 	end
 	def find_palindrom_made_from_two_number digits
 		max = ("9" * digits).to_i
